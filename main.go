7a7fd1e957b8915d786f812e00e1e69a0ad88dce
happyyi(易罗阳) <buutuud@gmail.com>
2023-08-05 05:05:18 +0000

    add http_proxy

--- main.go
+++ main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
+	"net/http"
 
 	socks5 "github.com/armon/go-socks5"
 )
 
-func main() {
+func http_proxy() {
+	http.HandleFunc("/", proxyHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+	req, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	req.Header = r.Header
+
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+}
+
+func socks5_proxy() {
 	// 创建一个新的SOCKS5服务器实例
 	conf := &socks5.Config{}
 	server, err := socks5.New(conf)
@@ -31,4 +65,9 @@ func main() {
 		// 将连接交给SOCKS5服务器处理
 		go server.ServeConn(conn)
 	}
+}
+
+
+func main() {
+	http_proxy()
 }
